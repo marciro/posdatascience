9c4cf670fc90569d8f0076c8280f943ceb26967c
Marcelo Rodrigues <marciro@gmail.com>
2020-04-04 17:26:14 -0300

    Implementação do trabalho

--- nosql/redis/redisgo/redisgo.py
+++ nosql/redis/redisgo/redisgo.py
@@ -1,5 +1,9 @@
 import redis
+import random
 
+def create_setnumber(r):
+    for i in range(1,99):
+        r.sadd('set_number',i)
 
 # Conectar no Redis
 def connect_redis(p_host, p_port, p_password):
@@ -16,12 +20,27 @@ def create_repo(r):
     #r.set('foo','bar')
     #print(r.get('foo'))
 
+    for user in range(1,50):
+        create_card(user,r)
+
+
+
+    #r.set('cartela_'+str(i), bingo_numbers)
+
+def create_card(u,r):
+    create_setnumber(r)
+    #srandmember retorna uma lista de string
+    srandmember = r.srandmember('set_number', 15)
+    for i in range(1,15):
+        rand_num = srandmember[i]
+        #Enquanto o número já existir no conjunto, sorteia o próximo, senão prossegue
+        #if(rand_num  )
+        #Adiciona ao card que está sendo construido.
+        r.sadd('card_'+str(u),srandmember[i])
 
 
 def main():
-    redis = connect_redis("localhost", 6379, "")
+    redis = connect_redis('localhost', 6379, '')
     create_repo(redis)
-
-
 
 main()
