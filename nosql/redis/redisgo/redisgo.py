e234912c728b20221291a34cf1be75ff71daf223
Marcelo Rodrigues <marciro@gmail.com>
2020-04-04 16:05:44 -0300

    Adição do readme

--- nosql/redis/redisgo/redisgo.py
+++ nosql/redis/redisgo/redisgo.py
@@ -1,7 +1,27 @@
 import redis
 
 
-r = redis.Redis(
-    host='localhost',
-    port=6379,
-    password='')
+# Conectar no Redis
+def connect_redis(p_host, p_port, p_password):
+    r = redis.Redis(
+        host=p_host,
+        port=p_port,
+        password=p_password)
+
+    return r
+
+
+# Criar database do bingo
+def create_repo(r):
+    #r.set('foo','bar')
+    #print(r.get('foo'))
+
+
+
+def main():
+    redis = connect_redis("localhost", 6379, "")
+    create_repo(redis)
+
+
+
+main()
