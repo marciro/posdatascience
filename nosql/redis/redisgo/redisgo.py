431c72948022b308c215678af780c7b63a476b97
Marcelo Cícero Rodrigues da Costa <marciro@gmail.com>
2020-04-26 17:56:43 -0300

    Finalização da implementação do trabalho de Redis

--- nosql/redis/redisgo/redisgo.py
+++ nosql/redis/redisgo/redisgo.py
@@ -2,7 +2,7 @@ import redis
 import random
 
 def create_setnumber(r):
-    for i in range(1,99):
+    for i in range(1,100):
         r.sadd("set:numbers",int(i))
 
 
@@ -20,21 +20,22 @@ def create_cards(r):
     create_setnumber(r)
     #srandmember retorna uma lista de string
 
-    for i in range(1,50):
+    for i in range(1,51):
         print("Gerando o cartão: " +"card:"+str(i))
         card = r.srandmember("set:numbers", 15)
         print("Números escolhidos: "+str(card))
-        for j in range (0,14):
-            print("Adicionando "+str(card[j]))
-            r.sadd("card:"+str(i),card[j])
+        for j in range (1,16):
+            print("Adicionando "+str(card[j-1]))
+            r.sadd("card:"+str(i),card[j-1])
 
         print(r.smembers("card:"+str(i)))
         r.sadd("available_cards",i)
 
 def clearing(redis):
     redis.delete("set:numbers")
-    for i in range(1,50):
+    for i in range(1,51):
         redis.delete("card:"+str(i))
+        redis.delete("player:"+str(i)+":score")
 
 
 def create_player(redis, i):
@@ -47,23 +48,30 @@ def create_player(redis, i):
 
 
 def create_players(redis):
-    for i in range(1,50):
+    for i in range(1,51):
         create_player(redis,i)
 
 
 def theres_winner(redis):
-    for i in range(1,50):
+    for i in range(1,51):
         player_score = redis.get("player:"+str(i)+":score")
-        print("Verificando score do jogador:"+str(player_score))
+        print("Verificando score do jogador player"+str(i)+": "+str(player_score))
         if player_score is not None and int(player_score) == 15:
-            print("Encontardo o vencedor:"+"player:"+str(i))
+            winners_name = redis.hget("player:"+str(i),"name")
+            winners_card = redis.hget("player:"+str(i),"card")
+            winners_numbers = redis.smembers(winners_card)
+            print("Encontrado o vencedor:" + "player:" + str(i))
+
+            print("Nome: "+str(winners_name))
+            print("Cartão: " + str(winners_card))
+            print("Números vencedores: " + str(winners_numbers))
             redis.set("winner", "player:"+str(i))
             return True
     return False
 
 
 def check_score(redis,number_drawed):
-    for i in range(1,50):
+    for i in range(1,51):
         card = redis.hget("player:"+str(i),"card")
         cardNumbers = redis.smembers(card)
         if number_drawed in cardNumbers:
@@ -80,9 +88,11 @@ def start_game(redis):
     # Enquanto não houver vencendor, sortear um número
     # Percorrer todas as cartelas e adicionar no score de cada jogador caso tenha o número em sua cartela
     # O jogador que alcançar o score de 15 primeiro é o vencedor.
+
+
     while(not theres_winner(redis)):
         number_drawed = draw_number(redis)
-        print(number_drawed)
+        print("Número sorteado: "+str(number_drawed))
         check_score(redis,number_drawed)
         
         
